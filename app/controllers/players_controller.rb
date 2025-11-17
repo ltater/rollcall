f333b64ccfa0de39ee41b35809dea8f9bb987491
Larissa Tater <13007905+ltater@users.noreply.github.com>
2025-11-17 10:07:14 -0600

    added player routes and views

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,36 @@
 class PlayersController < ApplicationController
+  before_action :set_player, only: %i[ show edit update destroy ]
+
   def index
     @players = Player.all
+  end
+
+  def show
+  end
+
+  def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to @player
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @player.destroy
+    redirect_to players_path
+  end
+
+  private def_set player
+    @player = Player.find(params[:id])
+  end
+
+  def player_params
+    params.expect(player: [ :name ])
   end
 end
