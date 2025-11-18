85f26bff7e50ececf88dc2de2f757ecef6fb7ed2
Larissa Tater <13007905+ltater@users.noreply.github.com>
2025-11-18 14:19:46 -0600

    Added Team table, controller, routes, views

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_17_171133) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_18_193504) do
   create_table "players", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
     t.string "name"
     t.string "phone"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 end
